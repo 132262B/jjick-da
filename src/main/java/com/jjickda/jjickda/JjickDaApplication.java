507d2a374284280682ad67701c17cd33e722b226
Igor <132262b@naver.com>
2022-09-19 19:32:42 +0900

    ✨ init project

---
+++ src/main/java/com/jjickda/jjickda/JjickDaApplication.java
@@ -0,0 +1,13 @@
+package com.jjickda.jjickda;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JjickDaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JjickDaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/jjickda/jjickda/JjickDaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jjickda.jjickda;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JjickDaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
