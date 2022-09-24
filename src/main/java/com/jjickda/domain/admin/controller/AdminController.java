65a31074f095a81f9a3f556d261a72d5a3ea1b9b
realomadia <hhun6840@naver.com>
2022-09-24 23:05:56 +0900

    [✨DEV] 관리자페이지 화면 연결 작업

--- src/main/java/com/jjickda/domain/admin/controller/AdminController.java
+++ src/main/java/com/jjickda/domain/admin/controller/AdminController.java
@@ -1,7 +1,23 @@
 package com.jjickda.domain.admin.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class AdminController {
+
+    @GetMapping("/adminPage")
+    public String AdminPage(Model model) {
+        return "main/admin/adminPage";
+    }
+    @GetMapping("/writeQuestion")
+    public String questionWrite(Model model){
+        return "main/admin/writeQuestion";
+    }
+
+    @GetMapping("/writeQuestionForm")
+    public String questionWriteForm(Model model){
+        return "main/admin/writeQuestionForm";
+    }
 }
