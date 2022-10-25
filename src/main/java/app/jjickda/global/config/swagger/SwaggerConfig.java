6c33a322e37eeca400a599d13985846e39d5dd95
Igor <132262b@naver.com>
2022-10-25 23:03:31 +0900

    [✨DEV] SwaggerConfig 설정 @value 로 변경, 설정 분리

--- src/main/java/app/jjickda/global/config/swagger/SwaggerConfig.java
+++ src/main/java/app/jjickda/global/config/swagger/SwaggerConfig.java
@@ -1,5 +1,6 @@
 package app.jjickda.global.config.swagger;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import springfox.documentation.builders.ApiInfoBuilder;
@@ -8,29 +9,43 @@ import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @Configuration
 public class SwaggerConfig {
+
+    @Value("${swagger.base-package}")
+    private String API_BASE_PACKAGE;
+
+    @Value("${swagger.path}")
+    private String API_PATH;
+
+    @Value("${swagger.title}")
+    private String API_TITLE;
+
+    @Value("${swagger.description}")
+    private String API_DESCRIPTION;
+
+    @Value("${swagger.version}")
+    private String API_VERSION;
 
     @Bean
     public Docket api() {
         return new Docket(DocumentationType.OAS_30)
                 .select() // ApiSelectorBuilder 생성
-                .apis(RequestHandlerSelectors.basePackage("app.jjickda.api")) // API 패키지 경로
-                .paths(PathSelectors.ant("/api/**")) // path 조건에 따라서 API 문서화
+                .apis(RequestHandlerSelectors.basePackage(API_BASE_PACKAGE)) // API 패키지 경로
+                .paths(PathSelectors.ant(API_PATH)) // path 조건에 따라서 API 문서화
                 .build()
                 .apiInfo(apiInfo()) // API 문서에 대한 정보 추가
                 .useDefaultResponseMessages(false) // swagger에서 제공하는 기본 응답 코드 설명 제거
-                //.ignoredParameterTypes(MemberInfo.class) //제외할 파라미터
+                //.ignoredParameterTypes(Mai.class) //제외할 파라미터
                 ;
     }
 
     private ApiInfo apiInfo() {
         return new ApiInfoBuilder()
-                .title("찍다 API 문서")
-                .description("API에 대해서 설명해주는 문서입니다.")
-                .version("1.0")
+                .title(API_TITLE)
+                .description(API_DESCRIPTION)
+                .version(API_VERSION)
                 .build();
     }
 }
