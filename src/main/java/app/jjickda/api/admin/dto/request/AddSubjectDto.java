bdf8dbf285d6278270642d6b990fe9debd6ea947
realomadia <hhun6840@naver.com>
2022-11-25 17:56:25 +0900

    [🚨FIX ] common.js positiveNumber 수정,  과목 등록 수정

--- src/main/java/app/jjickda/api/admin/dto/request/AddSubjectDto.java
+++ src/main/java/app/jjickda/api/admin/dto/request/AddSubjectDto.java
@@ -4,6 +4,7 @@ import io.swagger.annotations.ApiModelProperty;
 import lombok.*;
 import org.apache.ibatis.type.Alias;
 
+import javax.validation.constraints.Max;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Positive;
@@ -27,4 +28,9 @@ public class AddSubjectDto {
     @NotNull
     @Positive
     private float subjectCutOffScore;
+
+    @Max(50)
+    @NotNull
+    @Positive
+    private int subjectQuestionCnt;
 }
