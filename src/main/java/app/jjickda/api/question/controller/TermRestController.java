46ecd77b98732e88c55a42b1ef8e93e6a61eb940
AKANY99 <dlwlcjf558@naver.com>
2022-11-25 13:54:26 +0900

    [✨DEV] 화살표 함수로 변경,  Question 에서 Term 으로 클래스 이름 변경

--- src/main/java/app/jjickda/api/question/controller/TermRestController.java
+++ src/main/java/app/jjickda/api/question/controller/TermRestController.java
@@ -3,15 +3,12 @@ package app.jjickda.api.question.controller;
 import app.jjickda.api.question.dto.response.CertificateListDto;
 import app.jjickda.api.question.dto.response.ExamListDto;
 import app.jjickda.api.question.dto.response.SubjectListDto;
-import app.jjickda.api.question.service.QuestionService;
-import app.jjickda.domain.user.dto.request.SignUpDto;
+import app.jjickda.api.question.service.TermService;
 import app.jjickda.global.config.model.ApiResponse;
-import app.jjickda.global.utils.SessionUtil;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
-import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -20,30 +17,30 @@ import java.util.List;
 @Api(tags = "자격증선택 페이지 데이터 처리와 관련된 API.")
 @RestController
 @RequestMapping("/api")
-public class QuestionRestController {
+public class TermRestController {
 
-    private final QuestionService questionService;
+    private final TermService TermService;
 
-    public QuestionRestController(QuestionService questionService) { this.questionService = questionService; }
+    public TermRestController(TermService TermService) { this.TermService = TermService; }
 
     @ApiOperation("자격증데이터를 가져오는 API")
     @PostMapping("/certificate")
     public ResponseEntity<ApiResponse<List<CertificateListDto>>> questionSelect() {
-        List<CertificateListDto> certificateListDto = questionService.questionSelect();
+        List<CertificateListDto> certificateListDto = TermService.questionSelect();
         return ResponseEntity.ok(new ApiResponse<>(certificateListDto));
     }
 
     @ApiOperation("과목데이터를 가져오는 API")
     @PostMapping("/subject/{subIdx}")
     public ResponseEntity<ApiResponse<List<SubjectListDto>>> subjectSelect(@PathVariable long subIdx) {
-        List<SubjectListDto> subjectListDto = questionService.subjectSelect(subIdx);
+        List<SubjectListDto> subjectListDto = TermService.subjectSelect(subIdx);
         return ResponseEntity.ok(new ApiResponse<>(subjectListDto));
     }
 
     @ApiOperation("회차데이터를 가져오는 API")
     @PostMapping("/exam/{subIdx}")
-    public ResponseEntity<ApiResponse<List<ExamListDto>>> examSelect(@PathVariable long subIdx) {
-        List<ExamListDto> examListDto = questionService.examSelect(subIdx);
+        public ResponseEntity<ApiResponse<List<ExamListDto>>> examSelect(@PathVariable long subIdx) {
+        List<ExamListDto> examListDto = TermService.examSelect(subIdx);
         return ResponseEntity.ok(new ApiResponse<>(examListDto));
     }
 
